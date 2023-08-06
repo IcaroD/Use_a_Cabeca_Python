791b2eecedd7f4fa32423d6501bd0543057dda12
ICARO DAVILA SANTOS <icaro.davila.santos@gmail.com>
2023-08-05 22:33:08 -0300

    Capítulo 2

---
+++ panic2.py
@@ -0,0 +1,11 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+tap = plist[-7:-9:-1]
+tap.extend(plist[-5:-7:-1])
+plist = plist[1:3]
+plist.extend(tap)
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
